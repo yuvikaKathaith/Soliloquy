04cf9a4221c14b3e62b294312fae14f2eda6e086
yuvikakathaith <yuvikakathaith3103@gmail.com>
2025-09-08 11:45:47 +0530

    object issue resolved

--- actions/public.js
+++ actions/public.js
@@ -1,22 +1,20 @@
 import { unstable_cache } from 'next/cache';
 
 export const getDailyprompt = unstable_cache(
-    async() => {
-        try {
-            const res = await fetch("https://api.adviceslip.com/advice", {
-                cache:"no-store"
-            })
-            const data = await res.json();
-            return data.slip.advice;
-        } catch (error) {
-            return {
-                success: false,
-                data: "What's on your mind today",
-            }
-        }
-    }, ["daily-prompt"],
-    {
-        revalidate:43200,
-        tags: ["daily-prompt"]
+  async () => {
+    try {
+      const res = await fetch("https://api.adviceslip.com/advice", {
+        cache: "no-store",
+      });
+      const data = await res.json();
+      return data.slip.advice;
+    } catch (error) {
+      return "What's on your mind today"; 
     }
-)
+  },
+  ["daily-prompt"],
+  {
+    revalidate: 43200, // 12 hours
+    tags: ["daily-prompt"],
+  }
+);
